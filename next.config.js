de6247939f221a2c8eb64cd994b63e1fb5593f5a
JiaAnTW <at5lp6andy@gmail.com>
2022-07-29 19:37:46 +0800

    ci: update eslint and webpack setting

--- next.config.js
+++ next.config.js
@@ -3,11 +3,19 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack: (config) => {
+        config.module.resolve.push({
+            alias: {
+                '~': path.resolve(__dirname, '.'),
+            },
+        });
+        return config;
+    },
 };
 
 module.exports = nextConfig;
