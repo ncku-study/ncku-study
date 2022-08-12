ac3ac7565d24cd5f407fe91c0d4bad6f2e19c561
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2022-08-12 19:22:20 +0800

    fix: `sidebar` typo

--- src/components/GlobalLayout/useMediaEffect.ts
+++ src/components/GlobalLayout/useMediaEffect.ts
@@ -2,14 +2,14 @@ import { useAppDispatch } from '@/redux/hooks';
 import { useEffect } from 'react';
 
 import { useMedia } from '@/utils/index';
-import { updateSidebarStatus } from '~/src/redux/actions/layout';
+import { updateSideBarStatus } from '~/src/redux/actions/layout';
 
 export default function useMediaEffect() {
     const device = useMedia();
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if (device === 'PC') dispatch(updateSidebarStatus(true));
-        else dispatch(updateSidebarStatus(false));
+        if (device === 'PC') dispatch(updateSideBarStatus(true));
+        else dispatch(updateSideBarStatus(false));
     }, [device, dispatch]);
 }
