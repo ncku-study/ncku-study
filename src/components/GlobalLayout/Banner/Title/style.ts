3f8367abdee07c74f3911b10ebc8054e34107bca
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2022-08-23 15:40:38 +0800

    refactor: recover searchbar with ts
    - remove redundant code
    - replace onClick handler with onMouseDown to simplify logic

--- src/components/GlobalLayout/Banner/Title/style.ts
+++ src/components/GlobalLayout/Banner/Title/style.ts
@@ -1,58 +1,60 @@
 import styled from '@emotion/styled';
 
-import SearchBar from '@/components/SearchBar';
+import SearchBar, { SearchBarProps } from '@/components/SearchBar';
 
-export const H1 = styled.h1`
-    margin: 0;
-    font-size: 1.7rem;
-    font-weight: 500;
-    width: 100%;
+export const H1 = styled('h1')({
+    width: '100%',
+    margin: 0,
 
-    @media (max-width: 992px) {
-        padding-left: 20px;
-        width: auto;
-        align-items: center;
-    }
-`;
+    fontSize: '1.7rem',
+    fontWeight: 500,
 
-export const H2 = styled.h2`
-    margin: 0;
-    font-size: 2.2rem;
-    font-weight: 600;
-    width: 100%;
+    '@media (max-width: 992px)': {
+        width: 'auto',
+        paddingLeft: '20px',
 
-    @media (max-width: 992px) {
-        padding-left: 20px;
-        width: auto;
-        align-items: center;
-    }
-`;
+        alignItems: 'center',
+    },
+});
 
-interface SearchProps {
-    isShow: boolean;
-}
+export const H2 = styled('h2')({
+    width: '100%',
+    margin: 0,
 
-export const Search = styled(SearchBar)<SearchProps>`
-    width: 550px;
-    height: 45px;
-    display: ${(props) => (props.isShow ? 'flex' : 'none')};
-    @media (max-width: 992px) {
-        flex-grow: 1;
-        width: 50%;
-        height: 35px;
-        padding-left: 25px;
-    }
+    fontSize: '2.2rem',
+    fontWeight: 600,
 
-    @media (max-width: 576px) {
-        width: 30%;
-        height: 35px;
-        padding-left: 25px;
-    }
-`;
+    '@media (max-width: 992px)': {
+        width: 'auto',
+        paddingLeft: '20px',
 
-export const Header = styled.header`
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-`;
+        alignItems: 'center',
+    },
+});
+
+export const StyledSearchBar = styled(SearchBar)<SearchBarProps>({
+    width: '550px',
+    height: '45px',
+    display: 'flex',
+
+    '@media (max-width: 992px)': {
+        width: '50px',
+        height: '35px',
+        paddingLeft: '25px',
+
+        flexGrow: 1,
+    },
+    '@media (max-width: 576px)': {
+        width: '30%',
+        height: '35px',
+        paddingLeft: '25px',
+    },
+});
+
+export const Header = styled('header')({
+    width: '100%',
+    display: 'flex',
+
+    flexWrap: 'wrap',
+    alignItems: 'center',
+});
