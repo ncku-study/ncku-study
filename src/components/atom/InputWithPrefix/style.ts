03e3e1dd1b70621ffed4cd48a0f3ffbbc061229c
Kyl23 <cklkailiang@gmail.com>
2022-08-26 06:32:03 +0000

    fix: eslint export default format error

--- src/components/atom/InputWithPrefix/style.ts
+++ src/components/atom/InputWithPrefix/style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+// eslint-disable-next-line import/prefer-default-export
 export const InputFieldWithPrefix = styled.div`
     display: inline-flex;
     align-items: flex-end;
